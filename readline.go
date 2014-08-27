17769cc5176456ffe1f8b24002af690f46457b2a
diebels727 <diebelsalternative@hotmail.com>
2014-08-27 09:37:53 -0400

    encapsulate.

--- readline.go
+++ readline.go
@@ -2,9 +2,19 @@ package readline
 import (
   "bufio"
   "os"
-  "strconv"
-  "strings"
 )
+
+type Readline struct {
+  File *os.File
+}
+
+func (r *Readline) Map(fn func(f string)) {
+  scanner := bufio.NewScanner(r.File)
+  for scanner.Scan() {
+    fn(scanner.Text())
+  }
+  return
+}
 
 func Map(path string,fn func(f string)) {
   //open input file
